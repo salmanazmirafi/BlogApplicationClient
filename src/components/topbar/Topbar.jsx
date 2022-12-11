7f7e5f6304f736116b2595736fc1573574e1c399
salmanazmirafi <116106498+salmanazmirafi@users.noreply.github.com>
2022-12-12 02:21:16 +0600

    Update

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -6,10 +6,10 @@ export default function Topbar() {
   return (
     <div className="top">
       <div className="topLeft">
-        <i className="topIcon fab fa-facebook-square"></i>
-        <i className="topIcon fab fa-instagram-square"></i>
-        <i className="topIcon fab fa-pinterest-square"></i>
-        <i className="topIcon fab fa-twitter-square"></i>
+        <i className="topIcon fab fa-facebook"></i>
+        <i className="topIcon fab fa-instagram"></i>
+        <i className="topIcon fab fa-pinterest"></i>
+        <i className="topIcon fab fa-twitter"></i>
       </div>
       <div className="topCenter">
         <ul className="topList">
